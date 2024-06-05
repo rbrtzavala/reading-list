78dcb5260bed2656fdf306ecb1f3a28df0810008
Robert Zavala <rbrtzavala@gmail.com>
2024-06-05 11:16:24 -0600

    connect BookCreate to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
+import BookCreate from './Components/BookCreate.js';
+
 function App() {
-  return <div>App</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log(`Add book with title: ${title}`)
+  };
+
+  const editBook = () => {
+
+  };
+
+  const deleteBook = () => {
+
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
